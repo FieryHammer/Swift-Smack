ab357ac6d0ffb37ec73994584fddf21adfd5155a
Horvath, Mate <Mate.Horvath.@misys.com>
2018-10-19 17:10:31 +0200

    🐣 FEATURE : AuthService connected + RoundedButton UIButton created

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -10,14 +10,39 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    @IBOutlet weak var usernameTxtFld: UITextField!
+    @IBOutlet weak var emailTxtFld: UITextField!
+    @IBOutlet weak var passwordTxtFld: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    @IBAction func createAccountBtnPressed(_ sender: Any) {
+        guard let email = emailTxtFld.text, emailTxtFld.text != "" else { return }
+        guard let password = passwordTxtFld.text, emailTxtFld.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("registered user!")
+            }
+            else {
+                print("fail")
+            }
+        }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func generateBackgroundBtnPressed(_ sender: Any) {
+        
+    }
+    
     @IBAction func closeBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
-    
-    
 }
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,9 +8,19 @@
 
 import Foundation
 
-//Segues
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+
+// Segues
 let LOGIN_SEGUE = "loginVC"
-
 let CREATE_ACCOUNT_SEGUE = "createAccountVC"
-
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL_KEY = "userEmail"
+
+// URL Constants
+let BASE_URL = "https://my-chatty-chat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
